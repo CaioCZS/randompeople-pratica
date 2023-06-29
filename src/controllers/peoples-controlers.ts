7ba87f93c9ef11196c09fd064d12b0d8f541e5b6
Caio <caiozs21@gmail.com>
2023-06-29 19:50:08 -0300

    add utils layer

--- src/controllers/peoples-controlers.ts
+++ src/controllers/peoples-controlers.ts
@@ -1,11 +1,17 @@
 import { Request,Response } from "express"
 import { serivceGetPeople } from "../services/people-service"
+import * as utils from "../utils/randomNumber"
+import httpStatus from "http-status"
 
 export async function getPeople(req: Request,res:Response){
+    try{
     const {rows: peoples} = await serivceGetPeople()
-    const randomNumber:number = Math.floor(Math.random()*peoples.length)
-
+    const randomNumber = utils.randomNumber(peoples.length)
     res.send(peoples[randomNumber])
+
+   }catch(err){
+    res.status(httpStatus.INTERNAL_SERVER_ERROR).send(err)
+   }
 }
 
 
---
+++ src/utils/randomNumber.ts
@@ -0,0 +1,3 @@
+export function randomNumber(num : number){
+    return Math.floor(Math.random()*num)
+}
